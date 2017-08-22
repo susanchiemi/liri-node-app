c147be4ee188234a617316c7462f2dd36f72e92c
susan <Wormley5@aol.com>
2017-08-22 19:17:36 -0400

    added lost keys

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'Nwdri33mtYvewj2LksIQ09Pju',
+  consumer_secret: 'FxsDBZ0YDOjzjPJIPCMnEWXULF9YvtZnSwlRARW0qlPFL4JwbW',
+  access_token_key: '880589144682057732-sVrYTBjr2VgaiB93kHy8GLjg6mIMYDd',
+  access_token_secret: 'BXnqiR2eXLEESHd05CkzugKWpUZhNsswt2oRpirbpcqMc',
+}
